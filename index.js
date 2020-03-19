96ff524d429cc668334ae066f263b6770d3d63a0
Morgan Dennithorne <morgan@pal.care>
2020-03-19 17:03:25 +1100

    fixed cflint path

--- index.js
+++ index.js
@@ -1,91 +1,84 @@
-const exec = require('child_process').exec;
-const gutil = require('gulp-util');
-const path = require('path');
-const through = require('through2');
-const _ = require('lodash');
+const exec = require("child_process").exec;
+const gutil = require("gulp-util");
+const path = require("path");
+const through = require("through2");
+const _ = require("lodash");
 
-module.exports = (options) => {
+module.exports = options => {
+  const defaultOptions = {
+    failOnError: true,
+    quiet: true,
+    folder: "",
+    logError: true,
+    maxBuffer: 10000000
+  };
 
-	const defaultOptions = {
-		failOnError: true,
-		quiet: true,
-		folder: '',
-		logError: true,
-		maxBuffer: 10000000
-	};
+  // Set the default options
+  _.defaults(options, defaultOptions);
 
-	// Set the default options
-	_.defaults(options, defaultOptions)
+  return through.obj(function(file, encoding, callback) {
+    cflintPath = path.normalize("./node_modules/.bin/cflint");
 
-	return through.obj(function (file, encoding, callback) {
+    var command = cflintPath + " -stdout -json -quiet";
 
-		var cflintPath = './node_modules/gulp-cflint/node_modules/.bin/cflint';
+    if (options.logError) {
+      command += " -logerror";
+    }
 
-		if (process.platform === 'win32') {
-			cflintPath = path.normalize('./node_modules/.bin/cflint');
-		}
+    if (options.folder.length) {
+      var pathToUse = options.folder;
+      command += " -folder ";
+    } else {
+      var pathToUse = file.path;
+      command += " -file ";
+    }
 
-		var command = cflintPath + ' -stdout -json -quiet';
+    command += pathToUse;
 
-		if (options.logError) {
-			command += ' -logerror';
-		}
+    if (!options.quiet) {
+      gutil.log("cflint: " + pathToUse);
+    }
 
-		if (options.folder.length) {
-			var pathToUse = options.folder;
-			command += ' -folder ';
-		}else{
-			var pathToUse = file.path;
-			command += ' -file ';
-		}
+    exec(command, { maxBuffer: options.maxBuffer }, (err, stdout, stderr) => {
+      if (err) {
+        var error = new gutil.PluginError("gulp-cflint", err);
+        callback(error);
+      }
 
-		command += pathToUse;
+      var errorData = [];
 
-		if (!options.quiet) {
-			gutil.log('cflint: ' + pathToUse);
-		}
+      try {
+        errorData = JSON.parse(stdout).issues;
+      } catch (e) {
+        console.error(e);
+      }
 
-		exec(command, { maxBuffer: options.maxBuffer }, (err, stdout, stderr) => {
+      if (errorData.length > 0) {
+        errorData.forEach(element => {
+          element.locations.forEach(location => {
+            errorMessage = "CFLint error in: " + location.file;
 
-			if (err) {
-				var error = new gutil.PluginError('gulp-cflint', err);
-				callback(error);
-			}
+            gutil.log(
+              gutil.colors.magenta(
+                location.file + " " + location.line + ":" + location.column
+              )
+            );
+            gutil.log(gutil.colors.red(element.message));
+            gutil.log(gutil.colors.red(location.message));
+          });
+        });
 
-			var errorData = [];
+        if (options.failOnError) {
+          var error = new gutil.PluginError(
+            "gulp-cflint",
+            "CFLint error in: " + pathToUse
+          );
+        }
+      }
 
-			try {
-				errorData = JSON.parse(stdout).issues;
-			} catch (e) {
-				// Not valid JSON.
-			}
-
-			if (errorData.length > 0) {
-
-				errorData.forEach( (element) => {
-
-					element.locations.forEach( (location) => {
-
-						errorMessage = 'CFLint error in: ' + location.file
-
-						gutil.log(gutil.colors.magenta(location.file + ' ' + location.line + ':' + location.column));
-						gutil.log(gutil.colors.red(element.message));
-						gutil.log(gutil.colors.red(location.message));
-
-
-					});
-
-				});
-
-				if (options.failOnError) {
-					var error = new gutil.PluginError('gulp-cflint', 'CFLint error in: ' + pathToUse);
-				}
-			}
-
-			// take appropriate action then
-			this.push(file);
-			callback(error);
-		});
-
-	});
+      // take appropriate action then
+      this.push(file);
+      callback(error);
+    });
+  });
 };
